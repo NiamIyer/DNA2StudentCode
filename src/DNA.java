e4b53035612e6b6d9df76678c7364c44961180ff
niamiyer <niam.iyer@menloschool.org>
2025-10-19 20:21:44 -0700

    cs1 version implemented

--- src/DNA.java
+++ src/DNA.java
@@ -16,7 +16,25 @@ public class DNA {
      * TODO: Complete this function, STRCount(), to return longest consecutive run of STR in sequence.
      */
     public static int STRCount(String sequence, String STR) {
-
-        return 0;
+        int max = 0;
+        int curr = 0;
+        for (int i = 0; i < sequence.length(); i++) {
+            int k = i;
+            curr = 0;
+            while ((k + STR.length() < sequence.length()) && (sequence.substring(k,k + STR.length()).equals(STR))) {
+                curr ++;
+                k += STR.length();
+                if (k > sequence.length()) {
+                    if (curr > max) {
+                        return curr;
+                    }
+                    return max;
+                }
+            }
+            if (curr > max) {
+                max = curr;
+            }
+        }
+        return max;
     }
 }
